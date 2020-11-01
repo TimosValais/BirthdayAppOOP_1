0a772dd05e305357c8cdbe9cb15847b581caa529
Timos Valais <73531450+TimosValais@users.noreply.github.com>
2020-11-01 22:32:17 +0200

    Added Birthday Entry and Birthday test
    -Birthday test prints two entries
    -Birthday entry takes first and last names, telephones and dates and turns them into objects.

---
+++ BirthdayEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayAppOOP_1
+{
+    class BirthdayEntry
+    {
+        // characteristics of the class - fields
+        public string firstName;
+        public string lastName;
+        public string telephone;
+        public DateTime dateOfBirth;
+
+        // default constructor
+        public BirthdayEntry()
+        {
+            // put some sample values for the user to see, e.g.
+            // firstName = "NoName"
+            // ..
+            // dateOfBirth = "1900/01/01"
+            this.firstName = "NoName";
+            this.lastName = "NoName";
+            this.telephone = "00000000";
+            this.dateOfBirth = Convert.ToDateTime("1900/01/01");
+        }
+
+        // overloaded constructor
+        public BirthdayEntry(string firstName, string lastName, 
+                             string telephone, DateTime dateOfBirth)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.telephone = telephone;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        // overloaded constructor
+        public BirthdayEntry(string firstName)
+        {
+            // put some sample values for the user to see, e.g.
+            // ..
+            // dateOfBirth = "1900/01/01"
+            this.firstName = firstName;
+            this.lastName = "NoName";
+            this.telephone = "00000000";
+            this.dateOfBirth = Convert.ToDateTime("1900/01/01");
+        }
+
+        // methods
+        public override string ToString()
+        {
+            return ($"FirstName: {firstName}, LastName: {lastName}, " +
+                              $"   Telephone: {telephone}, Date Of Birth: {dateOfBirth}");
+        }
+
+
+    }
+}
---
+++ BirthdayTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayAppOOP_1
+{
+    class BirthdayTest
+    {
+        public void PrintTwoEntries(BirthdayEntry be, BirthdayEntry be2)
+        {
+            Console.WriteLine(be);
+            Console.WriteLine(be2);
+            //Console.WriteLine($"be {be.firstName}, {be.lastName}, " +
+            //                  $"   {be.telephone}, {be.dateOfBirth}");
+            //Console.WriteLine($"be2 {be2.firstName}, {be2.lastName}, " +
+            //                  $"   {be2.telephone}, {be2.dateOfBirth}");
+        }
+    }
+}
